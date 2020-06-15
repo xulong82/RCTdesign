329d2558ca1c1c6f493365fa2cf1c6bf3c0dfec9
Xulong Wang <xulong.wang@karyopharm.com>
2020-06-14 20:55:41 -0400

    14-Jun-20

--- rctdesign-tutorial.R
+++ rctdesign-tutorial.R
@@ -91,26 +91,3 @@ plot( interim3, dsnLbls=c("Implemented Design", "Original Design") )
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------
 
-y1 = gsDesign::nSurv(sided = 1, ratio = 1, alpha = 0.025, beta = 0.1, hr0 = 1, hr = 0.5, lambdaC = 1/5, eta = 1/10, gamma = 20, R = 8, T = 20)
-y1$d; y1$n; y1$T
-
-y2 = seqDesign(prob.model= "hazard", arms= 2, ratio= c(1,1), null.hypothesis= 1, alt.hypothesis= 0.5, test.type= "less", size= 0.025, power= 0.90)
-y2$parameters
-
-y3 = seqDesign(prob.model= "hazard", arms= 2, ratio= c(1,1), null.hypothesis= 1, alt.hypothesis= 0.5, test.type= "less", size= 0.025, power= 0.90,
-                accrualTime = 8, studyTime = 20, eventHazard = 1/5, dropoutHazard = 1/10)
-y3$parameters
-
-(d1 = seqDesign(prob.model= "mean", arms= 2, ratio= c(1,1), null.hypothesis= 0, alt.hypothesis= -10, sd= c(30,30), test.type= "less", size= 0.025, power= 0.90))
-(d1J2 = update(d1, nbr.analyses = 2))
-(d1J4 = update(d1, nbr.analyses = 4))
-
-# How to specify stopping only for efficacy, but not for futility, and how would it affect the efficacy boundaries?
-(d1J4x = update(d1J4, early.stopping = "alternative"))
-plot(d1J4, d1J4x) # bounaries values for efficacy are relaxed when futility stopping was used 
-
-(d1J4.eval = seqEvaluate(d1J4))
-plot(d1J2, d1J4)
-
-# What exact are the different bounary shape parameters (the unified family of group sequential stopping rules)
-(d1J4y = update(d1J4, design.family = "OBF"))
